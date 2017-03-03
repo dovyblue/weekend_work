615788d3ef8385a4ba5d5a407fbf56cbe8714af2
Dov Bluming <dovybluming@gmail.com>
2017-03-03 16:32:09 -0500

    finished find_longest_word

--- algo/find_longest_word.rb
+++ algo/find_longest_word.rb
@@ -1,7 +1,19 @@
 # Complete the method called find_longest_word, which will accept a string as a parameter (usually a sentence), and return another string that will be the longest word in that sentence.
 
 def find_longest_word(sentence)
-
+  split_sentence = sentence.split
+  longest_word = nil
+  most_characters_currently = 0
+  
+  split_sentence.each do |word|
+    number_of_characters = word.length
+    if number_of_characters > most_characters_currently
+      longest_word = word
+      most_characters_currently = number_of_characters
+    end
+  end
+  
+  p longest_word
 end
 
 # Driver code - don't touch anything below this line.
